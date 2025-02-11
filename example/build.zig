ec87a24f0a6180b7701cc90af80879ca8ffef83c
andrewkraevskii <andrew.kraevskii@gmail.com>
2025-02-11 05:17:40 +0300

    Update to real this time

--- example/build.zig
+++ example/build.zig
@@ -15,7 +15,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "tracy-example",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
