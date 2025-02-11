79d24d656fe26af54627cf2237345154c19ca3b0
andrewkraevskii <andrew.kraevskii@gmail.com>
2025-02-11 05:53:18 +0300

    Yank fix from #7

--- build.zig
+++ build.zig
@@ -77,10 +77,14 @@ pub fn build(b: *std.Build) void {
         tracy_client.linkSystemLibrary("dbghelp");
         tracy_client.linkSystemLibrary("ws2_32");
     }
-    tracy_client.linkLibCpp();
+    if (target.result.abi != .msvc) {
+        tracy_client.linkLibCpp();
+    } else {
+        tracy_client.linkLibC();
+    }
     tracy_client.addCSourceFile(.{
         .file = tracy_src.path("./public/TracyClient.cpp"),
-        .flags = &.{},
+        .flags = if (target.result.os.tag == .windows) &.{"-fms-extensions"} else &.{},
     });
     inline for (tracy_header_files) |header| {
         tracy_client.installHeader(
